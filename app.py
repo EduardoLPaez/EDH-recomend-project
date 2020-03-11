dbd4188807a2604ca3bf884d463f263e64aa1424
EduardoLPaez <edpaez54g5@gmail.com>
2020-03-11 13:59:57 -0400

    inproved the streamlit app flow. added basic skeleton for analysis section.

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ def top_commanders(in_, frame = data): # filters commander collor identities
 
 
 def main():
-    menu = ['start','color explination','EDH','color selection', 'card analysis']
-    submenu = ['color split', '']
+    menu = ['start','explinations','color selection', 'card analysis']
+    analisys_menu = ['start', 'types distribution']
     commanders = list(np.append(np.array(['chose']), data.query('is_commander == 1')['name'].sort_values()))
 
     choises = st.sidebar.selectbox('page', menu) # mor things to add
@@ -72,30 +72,71 @@ def main():
             as a format
         ''')
         #if st.button('Random'):
-    if choises == 'EDH':# remmber to pass writen text by text editor for spelling mistakes..
-        st.title('EDH as a format')
-        st.markdown('''
-        the EDH format (also known as commander or Eldr Dragon Highlander), is an interesting format with simple rules.\n
-        - first. all decks must have a commander
-            - commanders must be legendary creatures.
-            - some exeptions may apply. Said exeptions mention the clauses of their use in the card effects.
-            - all commanders start the game in the command zone.
-            - they can be cast from there whenever you could cast a creature. 
-            - if the commander would change location (i.e. field to grave, grave to exile, field to deck, ect.) you may instead have it go to commander zone instead.
-            - every time the commander is cast from the command zone after the first, the commander cost a cumulative 2 colorless more.(i.e. 2 after first, then 4,6 ,8, so on and soforth)
-        - decks must be 100 cards. including the commander(s)
-        - cards within the deck must be within the commander's color identity.
-            - i.e. if the comander's color identity is Red Blue. all cards must have a color identity of
-                - Red, 
-                - Blue,
-                - Red + Blue
-                - colorless 
-        - cards that generate colored mana can still generate mana of any color. but cannot have a color identity outside the commander's 
-        - life totals start at 40.
-        - there is no cap in the total number of players. 
-        - if the number of partisipating players is greater than 2, all players draw 1 on their first turn.
-        - most other rules are as normall in MTG.
-        ''')                   
+    if choises == 'explinations':
+        st.title('color and format explibations')
+        bar_choice = st.selectbox('section', ['color explination', 'EDH'])
+        if bar_choice == 'EDH':# remember to pass writen text by text editor for spelling mistakes..
+            st.title('EDH as a format')
+            st.markdown('''
+            the EDH format (also known as commander or Eldr Dragon Highlander), is an interesting format with simple rules.\n
+            - first. all decks must have a commander
+                - commanders must be legendary creatures.
+                - some exeptions may apply. Said exeptions mention the clauses of their use in the card effects.
+                - all commanders start the game in the command zone.
+                - they can be cast from there whenever you could cast a creature. 
+                - if the commander would change location (i.e. field to grave, grave to exile, field to deck, ect.) you may instead have it go to commander zone instead.
+                - every time the commander is cast from the command zone after the first, the commander cost a cumulative 2 colorless more.(i.e. 2 after first, then 4,6 ,8, so on and soforth)
+            - decks must be 100 cards. including the commander(s)
+            - cards within the deck must be within the commander's color identity.
+                - i.e. if the comander's color identity is Red Blue. all cards must have a color identity of
+                    - Red, 
+                    - Blue,
+                    - Red + Blue
+                    - colorless 
+            - cards that generate colored mana can still generate mana of any color. but cannot have a color identity outside the commander's 
+            - life totals start at 40.
+            - there is no cap in the total number of players. 
+            - if the number of partisipating players is greater than 2, all players draw 1 on their first turn.
+            - most other rules are as normall in MTG.
+            ''')   
+        if bar_choice == 'color explination': # need to add exerp for blue and green
+            st.title('color explination')
+            st.subheader('Black')
+            st.markdown('''
+            Black is the color of power, death, and ambition. It uses any and all posible resources to gain the upperhand in a game.
+            \nmost cards in the color use the life total, hand, deck, or even other creatures on the field or off it as a resource to be
+            \nexpended for further advantage. when mixed with other colors, the result tends to be more specialized on using
+            \none resourece, genneraly black white focuses on using lifetotal, black blue abuses the hand and deck, black red focuses on creatures 
+            \nand sacrefice, and black green focuses on recurring the graveyard at nauseum. 
+            \nblack's weackness lies in its inability to deal with non creature permanents such as artifacts and enchanments. its goal tends to be to
+            \nout value its opponents before the nonliving parts of the decks overwelm it.
+            ''')
+            st.subheader('White')
+            st.markdown('''
+            \nWhite is the color of healing, enchanment, and token armies. It specialises on fielding large numbers of smaller creatures and \naltering the rules of 
+            engament to benefit them. It is filled with cards that heal the player, and keep creatires alive against all ods,\n or earradicate everithing for aclean 
+            slate. when mixed into other colors white genneraly bolster some aspect of the color.\n white black focuses on modifiying the life total to your advantage,
+            white blue has some of the strongest and most encompasing enchanments,\n white green can field massive armies of tokens, and white red
+            is ussually associated with equipment artifacts. 
+            \nwhite's weackness lies in its speed. Its deffencive nature has a tendency to slow it down \nthough its versatily and ability to whipe the board clean
+            if nessesary compensate for it. 
+            ''')
+            st.subheader('Red')
+            st.markdown("""
+            Red is the color of emotion, destruction, and freedom. it hits the board fast, and its enemies faster. \nas a color it has the largest asortment of
+            damaging options of any color, rainging from creatures to fire and lightning spelles, to creature effects, \nand a pile of artifacts and artifact supporrt
+            for good measure. when mixed with other colors it tends to add a good degree of speed without messing with the other color most the time.
+            \nblack red focus on sacrefising smaller creatures to burn away ate the enemy, white red on building armies and equiping them with artifacts to\n burn away at their enemies,
+            red green on rushing green's ussual colosi to board even earlier to burn away their enemies,\n and red blue on the largest most conveluded conflagration imaginable.
+            to burn away their enemies....
+            \nOver all red is weack to its own speed. if an enemy is not dead when red runs out of steam,\n then there is little red can do.
+            """)
+            st.subheader('Blue')
+            st.markdown('''
+            ''')
+            st.subheader('Green')
+            st.markdown(''' 
+            ''')                
     if choises == 'color selection':# need to add check for vaiable plainwalker commanders
         st.title('Commander colors')
         st.markdown('''- all decks in the EDH format have a comander. the commanders color \nidentity determines what cardscan be added to it
@@ -114,46 +155,14 @@ def main():
             - {el}:  
             - {uri_return[1][i]}
             ''')
-    if choises == 'color explination': # need to aad exp for blue and green
-        st.title('color explination')
-        st.subheader('Black')
-        st.markdown('''
-        Black is the color of power, death, and ambition. It uses any and all posible resources to gain the upperhand in a game.
-        \nmost cards in the color use the life total, hand, deck, or even other creatures on the field or off it as a resource to be
-        \nexpended for further advantage. when mixed with other colors, the result tends to be more specialized on using
-        \none resourece, genneraly black white focuses on using lifetotal, black blue abuses the hand and deck, black red focuses on creatures 
-        \nand sacrefice, and black green focuses on recurring the graveyard at nauseum. 
-        \nblack's weackness lies in its inability to deal with non creature permanents such as artifacts and enchanments. its goal tends to be to
-        \nout value its opponents before the nonliving parts of the decks overwelm it.
-        ''')
-        st.subheader('White')
-        st.markdown('''
-        \nWhite is the color of healing, enchanment, and token armies. It specialises on fielding large numbers of smaller creatures and \naltering the rules of 
-        engament to benefit them. It is filled with cards that heal the player, and keep creatires alive against all ods,\n or earradicate everithing for aclean 
-        slate. when mixed into other colors white genneraly bolster some aspect of the color.\n white black focuses on modifiying the life total to your advantage,
-        white blue has some of the strongest and most encompasing enchanments,\n white green can field massive armies of tokens, and white red
-        is ussually associated with equipment artifacts. 
-        \nwhite's weackness lies in its speed. Its deffencive nature has a tendency to slow it down \nthough its versatily and ability to whipe the board clean
-        if nessesary compensate for it. 
-        ''')
-        st.subheader('Red')
-        st.markdown("""
-        Red is the color of emotion, destruction, and freedom. it hits the board fast, and its enemies faster. \nas a color it has the largest asortment of
-        damaging options of any color, rainging from creatures to fire and lightning spelles, to creature effects, \nand a pile of artifacts and artifact supporrt
-        for good measure. when mixed with other colors it tends to add a good degree of speed without messing with the other color most the time.
-        \nblack red focus on sacrefising smaller creatures to burn away ate the enemy, white red on building armies and equiping them with artifacts to\n burn away at their enemies,
-        red green on rushing green's ussual colosi to board even earlier to burn away their enemies,\n and red blue on the largest most conveluded conflagration imaginable.
-        to burn away their enemies....
-        \nOver all red is weack to its own speed. if an enemy is not dead when red runs out of steam,\n then there is little red can do.
-        """)
-        st.subheader('Blue')
-        st.markdown('''
-        ''')
-        st.subheader('Green')
-        st.markdown(''' 
-        ''')
     if choises == 'card analysis': # see if we can predict cmc....
-        st.title('tobe made')
+        st.title('Genral analysis')
+        analysis_choice = st.selectbox('section', analisys_menu)
+        if analysis_choice == 'start':
+            st.subheader('Hello')
+            st.markdown('this section is for the analisys\n and exploration of the mtg data\n select a section from the bar above.\n and have fun. :D\n')
+        if analysis_choice == 'types distribution':
+            st.subheader('working on it')
     if commander != 'chose':
         st.title(f'{commander}')
         cards(commander)
