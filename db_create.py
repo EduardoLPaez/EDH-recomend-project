742c1780f796185eb393327fc6d1f56e4cd905a1
EduardoLPaez <edpaez54g5@gmail.com>
2020-09-19 11:40:36 -0400

    pandas treating the dang responce.json as a dict. again....

--- db_create.py
+++ db_create.py
@@ -5,18 +5,30 @@ import pandas as pd
 import numpy as np
 import os.path, time
 from datetime import datetime
+import requests
 
 
 # last time modified is0: os.path.ctime(), time interval is gonna be slated to be 3-4 months(likely 4)
 # this is to permit blcok rotation. return is in unix time.
+def date_chek():
+    last_mod = datetime.utcfromtimestamp(os.path.getmtime('AllPrintings.sqlite'))# works, 
+    current = datetime.utcnow()#gets curent date.
 
-last_mod = datetime.utcfromtimestamp(os.path.getmtime('AllPrintings.sqlite'))# works, 
-current = datetime.utcnow()#gets curent date.
+    if (current - last_mod).days >= 90:
+        return True
+    else:
+        return False
 
-if (current - last_mod).days >= 90:
-    print("\n It works!!")
-else:
-    print ('\nfailuer...') 
+def db_core_pull():
+    if date_chek() == True:
+        # pull from origin... need some time for this...
+        responce = requests.get('http://mtgjson.com/api/v5/AllPrintings.json')
+        # print(responce.json())
+        # test = pd.read_sql(responce)
+        # test.head(1)
+    else:
+        return
 
+db_core_pull()
 # bellow is creation time, -----to add to tollbelt later----- 
 # print("created: %s" % time.ctime(os.path.getctime('AllPrintings.sqlite')))
